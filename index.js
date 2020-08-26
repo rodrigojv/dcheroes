3a685541867be0baf76944d4875a5701208a5be1
Rodrigo Villalba <rodris.jvo@gmail.com>
2020-08-26 17:19:13 -0400

    Actualiza schema y agrega query por character

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
-const heroesDb = require("./db");
+const charactersDb = require("./db").get("characters");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Heroe" type defines the queryable fields for every heroe in our data source.
-  type Heroe {
+  # This "Character" type defines the queryable fields for every heroe in our data source.
+  type Character {
+    name: String!
+    description: String!
     descriptionSummary: String
-    description: String
-    name: String
+    alignment: String
+    type: [String]
+    facts: Facts
+  }
+
+  type Facts {
+    powers: String
+    alterEgo: String
+    additionalInfo: [String]
+    firstAppearence: Comic
+  }
+
+  type Comic {
+    url: String
+    title: String!
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each.
   type Query {
-    heroes: [Heroe]
+    characters: [Character]
+    character(name: String!): Character
   }
 `;
 
@@ -25,7 +39,10 @@ const typeDefs = gql`
 // schema.
 const resolvers = {
   Query: {
-    heroes: () => heroesDb.get("heroes").value(),
+    characters: () => charactersDb.value(),
+    character: (_, { name }) => {
+      return charactersDb.find({ name: name.toLowerCase() }).value();
+    },
   },
 };
 
