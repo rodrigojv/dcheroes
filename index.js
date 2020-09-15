89391be8ee019e7ed6155a6a222eba0d009232c7
Rodrigo Villalba <rodris.jvo@gmail.com>
2020-09-15 16:44:43 -0400

    Enable introspection

--- index.js
+++ index.js
@@ -182,7 +182,12 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
